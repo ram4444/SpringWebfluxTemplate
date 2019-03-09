13b903f740cba30c5781f6837a5ee519a1d8217e
ram4444 <ramwt4444@gmail.com>
2019-03-10 00:09:51 +0800

    Rewrite cruelbyfuel, now return JSONObject and JSONArray in format

--- src/main/kotlin/controller/FuelController.kt
+++ src/main/kotlin/controller/FuelController.kt
@@ -4,13 +4,18 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.ResponseResultOf
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.fuel.json.responseJson
 import com.github.kittinunf.result.Result
 import mu.KotlinLogging
+import org.json.JSONArray
+import org.json.JSONObject
 import org.springframework.web.bind.annotation.*
 import java.net.InetSocketAddress
 import java.net.Proxy
+import java.util.*
 
 @RestController
 class FuelController() {
@@ -71,8 +76,9 @@ class FuelController() {
         return mapOf("data" to rtn)
     }
 
-    @PostMapping("/curlbyfuel")
-    fun curlByfuel(@RequestHeader method: String, @RequestHeader url: String, @RequestBody body: String): Map<String, Any> {
+    @ResponseBody
+    @PostMapping("/curlbyfuel", produces=["application/json"])
+    fun curlByfuel(@RequestHeader method: String, @RequestHeader url: String, @RequestBody body: String): String {
         //JSON is in the @RequestBody
         logger.info { "Curl is called by Fuel" }
         logger.debug { "method:" }; logger.debug { method }
@@ -88,73 +94,80 @@ class FuelController() {
         */
 
         val bodymap: Map<String, Map<String, Any>> = jacksonObjectMapper().readValue(body.trim())
+        val fuelcurlHeader_map: Map<String, Any>? = bodymap.get("header")
+        val fuelcurlBody_map: Map<String, Any>? = bodymap.get("body")
+        //TODO: A better way to initialize responResultofString
+        var responResultofString: ResponseResultOf<String> = Fuel.get("http://127.0.0.1").responseString()
 
         if (method.toLowerCase().equals("get")) {
-            // Get the header(to be sent) from the body
             //TODO: Implement Param
-            val fuelcurlHeader_map: Map<String, Any>? = bodymap.get("header")
+
 
             if (null != fuelcurlHeader_map) {
-                // Header does exist
-                val (request, response, result) = Fuel.get(url).header(fuelcurlHeader_map).responseString()
                 logger.info { "GET with Header" }
-                logger.debug { "request:" }; logger.debug { request }
-                logger.debug { "response:" }; logger.debug { response }
-                logger.debug { "result:" }; logger.debug { result }
-                return mapOf("rtn_code" to response.statusCode,
-                        "response" to response.responseMessage,
-                        "result" to result.get()
-                )
+                responResultofString= Fuel.get(url).header(fuelcurlHeader_map).responseString()
+
             } else {
-                // Header does not exist
-                val (request, response, result) = Fuel.get(url).responseString()
                 logger.info { "GET without Header" }
-                logger.debug { "request:" }; logger.debug { request }
-                logger.debug { "response:" }; logger.debug { response }
-                logger.debug { "result:" }; logger.debug { result }
-                return mapOf("rtn_code" to "success", "response" to response.data, "result" to result.get())
+                responResultofString = Fuel.get(url).responseString()
             }
-
         } else if (method.toLowerCase().equals("post")) {
-            /* Post method*/
             // Get the header and body (to be sent) from the body
-            val fuelcurlHeader_map: Map<String, Any> = bodymap.get("header").orEmpty()
-            val fuelcurlBody_map: Map<String, Any>? = bodymap.get("body")
-
             if (null != fuelcurlBody_map) {
-                val fuelcurlBody_listpairstrany: List<Pair<String, Any?>>? = fuelcurlBody_map.toList()
-                val (request, response, result) = Fuel.post(url)
-                        .header(fuelcurlHeader_map)
+                logger.info { "POST with Request Body" }
+                responResultofString = Fuel.post(url)
+                        .header(fuelcurlHeader_map.orEmpty())
                         .body(jacksonObjectMapper().writeValueAsString(fuelcurlBody_map))
                         .responseString()
-
-                logger.info { "POST with Request Body" }
-                logger.debug { "request:" }; logger.debug { request }
-                logger.debug { "response:" }; logger.debug { response }
-                logger.debug { "result:" }; logger.debug { result }
-
-                return mapOf("rtn_code" to response.statusCode,
-                        "response" to response.responseMessage,
-                        "result" to result
-                )
             } else {
-                val (request, response, result) = Fuel.post(url)
-                        .header(fuelcurlHeader_map)
+                logger.info { "POST without Request Body" }
+                responResultofString = Fuel.post(url)
+                        .header(fuelcurlHeader_map.orEmpty())
                         .responseString()
                 logger.info { "POST without Request Body" }
-                logger.debug { "request:" }; logger.debug { request }
-                logger.debug { "response:" }; logger.debug { response }
-                logger.debug { "result:" }; logger.debug { result }
-
-                return mapOf("rtn_code" to response.statusCode,
-                        "response" to response.responseMessage,
-                        "result" to result
-                )
             }
         } else {
             logger.error { "Other method is not support" }
-            return mapOf("rtn_code" to "fail", "msg" to "Other methmod is not allow")
+            //return mapOf("rtn_code" to "fail", "msg" to "Other methmod is not allow")
+            var jsonRtn = JSONObject()
+            jsonRtn.put("rtn_code", "500")
+            jsonRtn.put("response", "Other method is not support")
+            jsonRtn.put("result", JSONObject())
+            return jsonRtn.toString()
         }
+        val request = responResultofString.first
+        val response = responResultofString.second
+        val result = responResultofString.third
+
+        logger.debug { "request:" }; logger.debug { request.toString() }
+        logger.debug { "response:" }; logger.debug { response.toString() }
+        logger.debug { "result:" }; logger.debug { result.get() }
+
+        var jsonRtn = JSONObject()
+        jsonRtn.put("rtn_code", response.statusCode.toString())
+        jsonRtn.put("response", response.responseMessage)
+
+        if (result.get()[0].equals('{')) {
+            try {
+                jsonRtn.put("result", JSONObject(result.get()).toMap())
+            } catch (e: Exception) {
+                logger.error { "JSONObject conversion error" }
+                jsonRtn.put("result", JSONObject())
+            }
+        } else if (result.get()[0].equals('[')) {
+            try {
+                jsonRtn.put("result", JSONArray(result.get()))
+            } catch (e: Exception) {
+                logger.error { "JSONArray conversion error" }
+                jsonRtn.put("result", JSONObject())
+            }
+        } else {
+            logger.debug { result.get()[0] }
+            logger.error { "Not begin with brace [ or { " }
+            jsonRtn.put("result", JSONObject())
+        }
+
+        return jsonRtn.toString()
     }
 
 }
